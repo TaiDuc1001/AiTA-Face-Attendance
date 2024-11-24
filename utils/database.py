d2ff5c6d918616bc36be7868068d8ac6f0466f49
taiduc1001 <phantaiduc2005@gmail.com>
2024-11-24 18:34:53 +0700

    Fix: search in create

--- utils/database.py
+++ utils/database.py
@@ -17,21 +17,27 @@ def create_data_object(
         client: Client) -> None:
     schema = client.schema.get()
     class_names = [c['class'] for c in schema['classes']]
-    assert class_name in class_names, print(f'[bold red]Class {class_name} does not exist[/bold red]. Please choose from {class_names}')
+    if class_name not in class_names:
+        raise ValueError(
+            f"[bold red]Class {class_name} does not exist[/bold red]. "
+            f"Please choose from {class_names}")
 
     identifier = identifier.lower()
     schema = client.schema.get()[class_name]
     properties = [p['name'] for p in schema['properties']]
-    assert identifier in properties, print(f'[bold red]Property {identifier} does not exist in {class_name}[/bold red]. Please choose from {properties}')
+    if identifier not in properties:
+        raise ValueError(
+            f"[bold red]Property {identifier} does not exist in "
+            f"{class_name}[/bold red]. Please choose from {properties}")
 
-    name = data_object[identifier]
-    results = client.query.get(
-        class_name, [identifier, '_additional {id}']
-    ).with_where({
+    where_filter = {
         'path': [identifier],
         'operator': 'Equal',
-        'valueText': name
-    }).do()
+        'valueText': data_object[identifier]
+    }
+    results = client.query.get(
+        class_name, [identifier, '_additional {id}']
+    ).with_where(where_filter).do()
     
     if len(results['data']['Get'][class_name]) != 0:
         toDelID = results['data']['Get'][class_name][0]['_additional']['id']
