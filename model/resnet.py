55aa371ca72b9c1ecf05da963d0b26846fb5e77b
taiduc1001 <phantaiduc2005@gmail.com>
2024-11-24 22:17:32 +0700

    Remove cfg models

--- model/resnet.py
+++
@@ -1,8 +0,0 @@
-# from facenet_pytorch import InceptionResnetV1
-from .config import config
-
-if config['Recognizer']['name'] == 'resnet':
-    # model = InceptionResnetV1(pretrained='vggface2').eval()
-    model = None
-else:
-    raise ValueError('Invalid recognizer name')
