604acf8441791b749596aa2b065ae1c4c93efb39
taiduc1001 <phantaiduc2005@gmail.com>
2024-11-24 22:16:36 +0700

    Feat: rm model cfg

--- config/__init__.py
+++ config/__init__.py
@@ -1,29 +1,15 @@
 from .base import BaseConfig
 import yaml
-from model import resnet, yolo11n
 
 config_path = 'config.yaml'
 with open(config_path, 'r') as file:
     config = yaml.safe_load(file)
 
 class DatabaseConfig(BaseConfig):
-    def __init__(self, **kwargs):
-        super().__init__(config['VectorDatabase'])
+    def __init__(self, type: str, **kwargs):
+        super().__init__(config[type])
         for key, value in kwargs.items():
             setattr(self, key, value)
 
-class DetectorConfig(BaseConfig):
-    def __init__(self, **kwargs):
-        super().__init__(config['Detector'])
-        for key, value in kwargs.items():
-            setattr(self, key, value)
-
-class RecognizerConfig(BaseConfig):
-    def __init__(self, **kwargs):
-        super().__init__(config['Recognizer'])
-        for key, value in kwargs.items():
-            setattr(self, key, value)
-
-db_cfg = DatabaseConfig()
-detector_cfg = DetectorConfig(model=yolo11n)
-recognizer_cfg = RecognizerConfig(model=resnet)
+vecdb_cfg = DatabaseConfig(type='VectorDatabase')
+imgdb_cfg = DatabaseConfig(type='ImageDatabase')
