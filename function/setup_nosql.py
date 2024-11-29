155c6b90020b1fb738a35d749dd81e7e7f3094bd
taiduc1001 <phantaiduc2005@gmail.com>
2024-11-29 10:26:38 +0700

    Feat: make files to setup nosql and sql

---
+++ function/setup_nosql.py
@@ -0,0 +1,69 @@
+from manager import \
+    nosql_user_embed_left_schema, \
+    nosql_user_embed_middle_schema, \
+    nosql_user_embed_right_schema, \
+    Manager
+from config import nosql_cfg, objdb_cfg
+from .make_model import get_model
+from utils import detect_preprocess, recognize_postprocess
+
+import os
+from os.path import join, exists
+from os import listdir, remove, symlink
+import weaviate
+from insightface.app.common import Face
+
+
+def setup_nosql():
+    os.makedirs(nosql_cfg.persistence_data_path, exist_ok=True)
+    client = weaviate.Client(
+        embedded_options = weaviate.EmbeddedOptions(
+            host = nosql_cfg.host,
+            port = nosql_cfg.port,
+            persistence_data_path=nosql_cfg.persistence_data_path
+        )
+    )
+    manager = Manager(client=client)
+    for cls_name in manager.class_names:
+        client.schema.delete_class(cls_name)
+
+    manager.create_nosql_table(nosql_user_embed_left_schema)
+    manager.create_nosql_table(nosql_user_embed_middle_schema)
+    manager.create_nosql_table(nosql_user_embed_right_schema)
+
+    detector = get_model('Detector')
+    recognizer = get_model('Recognizer')
+
+    for person in listdir(objdb_cfg.names[objdb_cfg.name]):
+        person_dir = join(objdb_cfg.names[objdb_cfg.name], person)
+        embeddings = []
+        for face in (listdir(person_dir)[1:4]):
+            face = join(person_dir, face)
+            img = detect_preprocess(face)
+            bboxes, kpss = detector.detect(img, max_num=0, metric='default', input_size=img.shape[:2])
+            
+            if len(bboxes) == 0:
+                print(f'No face detected in {join(person, face)}')
+                continue
+            if len(bboxes) > 1:
+                print(f'More than one face detected in {join(person, face)}')
+                continue
+
+            face = Face(bbox=bboxes[0, :4], kps=kpss[0], det_score=bboxes[0, 4])
+            embedding = recognizer.get(img, face)
+            embeddings.append(embedding)
+
+        embeddings = recognize_postprocess(embeddings)
+
+        user = {
+            'name': person,
+            'embeddings': embeddings
+        }
+
+        manager.new_row(
+            user_object=user,
+            identifier='code',
+            type='nosql'
+        )
+
+        print(f'User {person} added to the vector database')
---
+++ function/setup_sql.py
@@ -0,0 +1,40 @@
+from config import sql_cfg
+from schemas import \
+    role as sql_role_schema, \
+    status as sql_status_schema, \
+    user as sql_user_schema, \
+    attendance as sql_attendance_schema
+
+import os
+from os.path import join, exists
+import sqlite3
+from sqlite3 import Cursor
+import pandas as pd
+
+def create_table(c: Cursor,
+                 schema: str) -> None:
+    c.execute(schema)
+    print(f'{schema} created')
+    schema_name = schema.split()[2].lower()
+    load_sample_data(c, schema_name)
+
+def load_sample_data(c: Cursor,
+                     schema_name: str) -> None:
+    data_path = join(sql_cfg.sample_data, f'{schema_name}.csv')
+    if exists(data_path):
+        data = pd.read_csv(data_path, index_col=False)
+        data.to_sql(schema_name, c, if_exists='append', index=False)
+        print(f'{schema_name} loaded {len(data)} rows')
+    else:
+        print(f'{data_path} not found')
+
+
+def setup_sql():
+    os.makedirs(sql_cfg.persistence_data_path, exist_ok=True)
+    conn = sqlite3.connect(join(sql_cfg.persistence_data_path, sql_cfg.filename))
+    c = conn.cursor()
+    create_table(c, sql_role_schema)
+    create_table(c, sql_status_schema)
+    create_table(c, sql_user_schema)
+    create_table(c, sql_attendance_schema)
+    conn.commit()
