0708751b148dbfc3e288483f75ae2fe509e39ea3
taiduc1001 <phantaiduc2005@gmail.com>
2024-11-29 10:25:07 +0700

    Feat: apply singleton

--- config/__init__.py
+++ config/__init__.py
@@ -6,10 +6,17 @@ with open(config_path, 'r') as file:
     config = yaml.safe_load(file)
 
 class DatabaseConfig(BaseConfig):
-    def __init__(self, type: str, **kwargs):
-        super().__init__(config[type])
+    _instance = None
+    def __new__(cls, **kwargs):
+        if cls._instance is None:
+            cls._instance = super(DatabaseConfig, cls).__new__(cls)
+        return cls._instance
+    
+    def __init__(self, **kwargs):
+        super(DatabaseConfig, self).__init__(config['database'], **kwargs)
         for key, value in kwargs.items():
             setattr(self, key, value)
 
-vecdb_cfg = DatabaseConfig(type='VectorDatabase')
-imgdb_cfg = DatabaseConfig(type='ImageDatabase')
+nosql_cfg = DatabaseConfig(type='NoSQLDatabase')
+objdb_cfg = DatabaseConfig(type='ObjectDatabase')
+sql_cfg = DatabaseConfig(type='SQLDatabase')
--- manager/__init__.py
+++ manager/__init__.py
@@ -1,63 +1,82 @@
-from .schemas import student_embedding_left_config, student_embedding_middle_config, student_embedding_right_config, student_info_config
-from weaviate import Client
+from schemas import \
+    nosql_user_embed_left_schema, \
+    nosql_user_embed_middle_schema, \
+    nosql_user_embed_right_schema, \
+    sql_role_schema, \
+    sql_status_schema, \
+    sql_user_schema, \
+    sql_attendance_schema
 from utils import create_class, create_data_object
-from .model import Student, generate_student_code
+from .model import UserInfo, UserEmbedding
+
+from typing import Union
+from weaviate import Client
 
 class Manager:
-    def __init__(self, client: Client):
-        self.client = client
-        self.class_names = [
-            'StudentEmbeddingLeft', 
-            'StudentEmbeddingMiddle', 
-            'StudentEmbeddingRight',
-            'StudentInfo', 
+    _instance = None
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super(Manager, cls).__new__(cls)
+        return cls._instance
+
+    def __init__(self, client: Client = None):    
+        if not hasattr(self, 'client'):
+            self.client = client
+
+    def get_class_names(self):
+        return [
+            nosql_user_embed_left_schema.class_name,
+            nosql_user_embed_middle_schema.class_name,
+            nosql_user_embed_right_schema.class_name,
         ]
     
-    def create_class(self, config):
+    def create_nosql_table(self, config):
         create_class(config, self.client)
 
-    def create_data_object(self,
-                           student_object: Student, 
-                           identifier: str) -> None:
-        student_info = {
-            'name': student_object.name,
-            'code': student_object.code,
-            'gender': student_object.gender
+    def new_row(
+            self,
+            user_object: Union[UserInfo, UserEmbedding], 
+            identifier: str,
+            type: str = 'nosql') -> None:
+        if type == 'nosql':
+            self.create_nosql_data_object(
+                user_object=user_object,
+                identifier=identifier
+            )
+
+    def create_nosql_data_object(
+            self,
+            user_object: UserEmbedding, 
+            identifier: str) -> None:
+        class_names = self.get_class_names()
+        user_embedding_left = {
+            'code': user_object.code,
         }
-        student_embedding_left = {
-            'code': student_object.code,
+        user_embedding_middle = {
+            'code': user_object.code,
         }
-        student_embedding_middle = {
-            'code': student_object.code,
-        }
-        student_embedding_right = {
-            'code': student_object.code,
+        user_embedding_right = {
+            'code': user_object.code,
         }
         create_data_object(
-            class_name=self.class_names[-1],
-            data_object=student_info,
+            class_name=class_names[0],
+            data_object=user_embedding_left,
             identifier=identifier,
-            vector=None,
+            vector=user_object.embeddings[0],
             client=self.client
         )
         create_data_object(
-            class_name=self.class_names[0],
-            data_object=student_embedding_left,
+            class_name=class_names[1],
+            data_object=user_embedding_middle,
             identifier=identifier,
-            vector=student_object.embeddings[0],
+            vector=user_object.embeddings[1],
             client=self.client
         )
         create_data_object(
-            class_name=self.class_names[1],
-            data_object=student_embedding_middle,
+            class_name=class_names[2],
+            data_object=user_embedding_right,
             identifier=identifier,
-            vector=student_object.embeddings[1],
+            vector=user_object.embeddings[2],
             client=self.client
         )
-        create_data_object(
-            class_name=self.class_names[2],
-            data_object=student_embedding_right,
-            identifier=identifier,
-            vector=student_object.embeddings[2],
-            client=self.client
-        )
+
