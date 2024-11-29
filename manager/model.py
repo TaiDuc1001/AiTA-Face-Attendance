4b66b7cc7e8d81464596f337e9c7e06f59abd649
taiduc1001 <phantaiduc2005@gmail.com>
2024-11-29 10:26:55 +0700

    Feat: split to info & embed

--- manager/model.py
+++ manager/model.py
@@ -4,29 +4,30 @@ import re
 import random
 import numpy as np
 
-def generate_student_code():
+def generate_user_code():
     suffix = ''.join(random.choices('0123456789', k=4))
     prefix = random.choice('QHCS')
     term = random.choice([str(i) for i in range(16, 20)])
     return f"{prefix}E{term}{suffix}"
 
-class Student(BaseModel):
-    name: str
+class UserInfo(BaseModel):
     code: str
+    name: str
     gender: bool
-    embeddings: List[np.ndarray]
-
+    role_code: str
     @field_validator('name')
     def name_must_be_alphabet(cls, value):
         if not all(part.isalpha() for part in value.split()):
             raise ValueError('Name must contain only alphabetic characters and spaces')
         return value
-    
+class UserEmbedding(BaseModel):
+    code: str
+    embeddings: List[np.ndarray]
     @field_validator('code')
     def code_must_be_valid(cls, value):
         pattern = r"^[QHCS]E(16|17|18|19)\d{4}$"
         if not re.match(pattern, value):
-            raise ValueError('Invalid student code format')
+            raise ValueError('Invalid user code format')
         return value
     
     class Config:
