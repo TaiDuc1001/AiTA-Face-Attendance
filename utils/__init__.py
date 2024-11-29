71171b1f7086615daacdf4d593665ad512a298f1
taiduc1001 <phantaiduc2005@gmail.com>
2024-11-29 10:26:14 +0700

    Chores: import preprocess detect

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,4 @@
 from .recognize import preprocess as recognize_preprocess, \
                         postprocess as recognize_postprocess
-from .detect import top1
+from .detect import top1, preprocess as detect_preprocess
 from .database import create_class, create_data_object
