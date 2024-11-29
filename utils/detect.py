47c6483037eae8b31bb0fcc718fd751308f8b55e
taiduc1001 <phantaiduc2005@gmail.com>
2024-11-29 10:25:55 +0700

    Feat: make preprocess for detect

--- utils/detect.py
+++ utils/detect.py
@@ -1,3 +1,7 @@
+from typing import Tuple, List
+import torch
+import cv2
+
 def top1(results):
     scores = []
     for result in results:
@@ -7,3 +11,15 @@ def top1(results):
     xyxy = map(int, list(xyxy))
     return xyxy
 
+def preprocess(image_path: str,
+               cropBox: List[int] = None,
+               img_size: Tuple[int, int] = (224, 224),
+               verbose: bool = False):
+    image = cv2.imread(image_path)
+    if cropBox:
+        image = image[cropBox[1]: cropBox[3], cropBox[0]: cropBox[2]]
+        if verbose:
+            print(f'[bold green]Found cropBox: {cropBox}[/bold green]. Cropping image...')
+    image = cv2.resize(image, img_size)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    return image
