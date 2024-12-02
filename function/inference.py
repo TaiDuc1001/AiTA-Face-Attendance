32f8e279a996a71d905bb28482c1cf0bd125aa4a
taiduc1001 <phantaiduc2005@gmail.com>
2024-12-02 21:49:38 +0700

    Feat: replace with camera manager

--- function/inference.py
+++ function/inference.py
@@ -7,6 +7,7 @@ import cv2
 import os
 import weaviate
 import argparse
+from function.live import CameraManager
 
 
 def main(config: dict):
@@ -25,53 +26,8 @@ def main(config: dict):
     detector = model_zoo.get_model(models_cfg['Detector']['names'][models_cfg.name])
     recognizer = model_zoo.get_model(models_cfg['Recognizer']['names'][models_cfg.name])
 
-    image_dir = imgdb_cfg['names'][imgdb_cfg.name]
-    for person in tqdm(listdir(image_dir)):
-        person_dir = join(image_dir, person)
-        embeddings = []
-        print(person)
-        for face in listdir(person_dir):
-            face = join(person_dir, face)
-            img = cv2.imread(face)
-            img = cv2.resize(img, (224, 224))
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            bboxes, kpss = detector.detect(img, max_num=0, metric='default',input_size=img.shape[:2])
-            face = Face(bbox=bboxes[0, :4], kps=kpss[0], det_score=bboxes[0, 4])
-            embedding = recognizer.get(img, face)
-            embeddings.append(embedding)
-
-        response = (
-            client.query
-            .get("UserEmbeddingLeft", ["code"])
-            .with_near_vector({"vector": embeddings[0]})
-            .with_limit(2).with_additional(["certainty", "distance"])
-            .do()
-        )
-
-        try:
-            for i in range(len(response['data']['Get']['UserEmbeddingLeft'])):
-                found = response['data']['Get']['UserEmbeddingLeft'][i]['code']
-                user_info_response = client.query.get(
-                    "UserInfo",  
-                    ["name", "code", "gender"]  
-                ).with_where({
-                    "path": ["code"],  
-                    "operator": "Equal",
-                    "valueText": found  
-                }).do()
-
-                try:
-                    user_name = user_info_response['data']['Get']['UserInfo'][0]['name']
-                    certainty = response['data']['Get']['UserEmbeddingLeft'][i]["_additional"]['certainty']
-                    distance = response['data']['Get']['UserEmbeddingLeft'][i]["_additional"]['distance']
-                    print(f"Found user info: {user_name} Certainty: {certainty} distance: {distance}")
-                except Exception as e:
-                    print("No user info found for the given ID")
-
-        except Exception as e:
-            print("No user found")
-            
-        break
+    camera_manager = CameraManager(config)
+    camera_manager.live()
 
 def parse_args():
     parser = argparse.ArgumentParser()
